cdcf0eb264aaaad2c7436c5f53e666b549acc435
Hegemon98 <conner.snow98.com>
2020-05-04 19:07:14 -0700

    Fixed my_services launch file functionality

--- my_services/src/service_client.py
+++ my_services/src/service_client.py
@@ -10,7 +10,10 @@ word_counter = rospy.ServiceProxy( # set up proxy
   'word_count', # service name
   WordCount     # service type
 )
-words = ' '.join(sys.argv[1:]) # parse args
+words_list_pass_1=[k for k in sys.argv if 'service_client.py' not in k]
+words_list_pass_2=[k for k in words_list if '__' not in k]
+words = ' '.join(words_list_pass_2) # parse args
 word_count = word_counter(words) # use service
 
 print(words+'--> has '+str(word_count.count)+' words')
+print(words_list)
