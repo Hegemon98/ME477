6407a1a7f15a22b0be36b6f873ad214247ed30f4
Hegemon98 <conner.snow98.com>
2020-05-04 20:49:42 -0700

    Revised server_client.py

--- my_services/src/service_client.py
+++ my_services/src/service_client.py
@@ -10,9 +10,8 @@ word_counter = rospy.ServiceProxy( # set up proxy
   'word_count', # service name
   WordCount     # service type
 )
-words_list_pass_1=[k for k in sys.argv if 'service_client.py' not in k]
-words_list_pass_2=[k for k in words_list if '__' not in k]
-words = ' '.join(words_list_pass_2) # parse args
+words_list=[k for k in sys.argv[1:] if '__' not in k]
+words = ' '.join(words_list) # parse args
 word_count = word_counter(words) # use service
 
 print(words+'--> has '+str(word_count.count)+' words')
